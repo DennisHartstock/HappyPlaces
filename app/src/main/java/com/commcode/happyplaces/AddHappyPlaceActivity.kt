b76f30c9df7bb276f5d989c0b3cfc95f1bbb96ab
DennsHartstock <dennis.hartstock@web.de>
2023-03-06 11:04:24 +0100

    implement DatePickerDialog

--- app/src/main/java/com/commcode/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/commcode/happyplaces/AddHappyPlaceActivity.kt
@@ -1,17 +1,46 @@
 package com.commcode.happyplaces
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.commcode.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddHappyPlaceBinding
+    private var calendar = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.etDate.setOnClickListener { getDate() }
+    }
+
+    private fun getDate() {
+        DatePickerDialog(
+            this@AddHappyPlaceActivity,
+            { _, year, month, dayOfMonth ->
+                calendar.set(Calendar.YEAR, year)
+                calendar.set(Calendar.MONTH, month)
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        ).show()
+    }
+
+    private fun updateDateInView() {
+        val dateFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(dateFormat, Locale.getDefault())
+        val date = sdf.format(calendar.time).toString()
+        (binding.etDate as TextView).text = date
     }
 }
