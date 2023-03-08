098bd22899c9548eb2bac17b4fe27c98181da293
DennsHartstock <dennis.hartstock@web.de>
2023-03-08 09:37:50 +0100

    add dexter library, implement storage permissions request

--- app/src/main/java/com/commcode/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/commcode/happyplaces/AddHappyPlaceActivity.kt
@@ -1,14 +1,26 @@
 package com.commcode.happyplaces
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.commcode.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import java.text.SimpleDateFormat
 import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), MultiplePermissionsListener {
 
     private lateinit var binding: ActivityAddHappyPlaceBinding
     private var calendar = Calendar.getInstance()
@@ -20,6 +32,34 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         setContentView(view)
 
         binding.etDate.setOnClickListener { getDate() }
+
+        binding.tvAddImage.setOnClickListener {
+            val addPictureDialog = AlertDialog.Builder(this)
+            val addPictureDialogItems =
+                arrayOf("Select photo from gallery", "Capture photo from camera")
+            addPictureDialog
+                .setTitle("Select action")
+                .setItems(addPictureDialogItems) { _, which ->
+                    when (which) {
+                        0 -> choosePhotoFromGallery()
+                        1 -> TODO()
+                    }
+                }
+                .show()
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        val storagePermissions = listOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        Dexter
+            .withContext(this)
+            .withPermissions(storagePermissions)
+            .withListener(this)
+            .onSameThread()
+            .check()
     }
 
     private fun getDate() {
@@ -42,5 +82,44 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         val sdf = SimpleDateFormat(dateFormat, Locale.getDefault())
         val date = sdf.format(calendar.time).toString()
         (binding.etDate as TextView).text = date
+    }
+
+    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+        if (report!!.areAllPermissionsGranted()) {
+            Toast.makeText(
+                this@AddHappyPlaceActivity,
+                "Storage Permissions are granted",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permissions: MutableList<PermissionRequest>?,
+        token: PermissionToken?,
+    ) {
+        showRationalDialogForPermissions()
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage(
+                "It looks like you have turned off Storage Permissions required " +
+                        "for this feature. It can be enabled under the Application Settings"
+            )
+            .setPositiveButton("go to application settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
