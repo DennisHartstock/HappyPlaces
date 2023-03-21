154e05135f9b47d09eba8f6def997696ee227668
DennsHartstock <dennis.hartstock@web.de>
2023-03-21 08:26:37 +0100

    add HappyPlaceDetailActivity

---
+++ app/src/main/java/com/commcode/happyplaces/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.commcode.happyplaces.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.commcode.happyplaces.databinding.ActivityHappyPlaceDetailBinding
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHappyPlaceDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHappyPlaceDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
